2588de5273f4193da3a9281ce495f59fa8fb58cd
Andrea Rosasco <andrearosasco.ar@gmail.com>
2024-02-26 23:14:19 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='teleop',
+    version='0.0.0',
+    license='Apache 2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description='teleop utilities',
+    author='Andrea Rosasco'
+    )
