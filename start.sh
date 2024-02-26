036828d5fc2159c80e2c957516c1f3bf7cb4a572
Andrea Rosasco <andrearosasco.ar@gmail.com>
2024-02-26 12:05:59 +0100

    changed image name

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-docker run --gpus=all -v "$(pwd)":/home/robotdataset/project -it \
+docker run --gpus=all -v "$(pwd)":/home/teleop/project -it \
 --gpus=all \
---env DISPLAY=:0  --env QT_X11_NO_MITSHM=1 \
+--env DISPLAY=$DISPLAY  --env QT_X11_NO_MITSHM=1 \
 --env XDG_RUNTIME_DIR=/root/1000 --env XAUTHORITY=/root/.Xauthority \
 --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 --network=host --privileged -v /dev/bus/usb:/dev/bus/usb \
---name robotdataset-ros2 ar0s/robotdataset:ros2 bash
+--name panda-teleop ar0s/panda-teleop bash
